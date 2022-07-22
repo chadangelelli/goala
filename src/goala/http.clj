6e7ad77cce28dc6b413a6dfe14f7cfff5aeb95d9
Chad Angelelli <chad.angelelli@gmail.com>
2022-07-21 23:15:27 -0500

    formats http lib

--- src/goala/http.clj
+++ src/goala/http.clj
@@ -8,34 +8,25 @@
 
 (defn start
   [opts]
-  
   (let [handler (atom (delay (:handler opts)))
         options (dissoc opts :handler)]
-    
     (util/log-info (str "Starting HTTP server on port " (:port options)))
-    
     {:handler handler
      :server (aleph/start-server (fn [req] (@@handler req)) options)}))
 
 (defn stop
   [{:keys [server]}]
-  
   (util/log-info "Stopping HTTP server")
-  
   (.close ^java.io.Closeable server))
 
 (defn suspend 
   [{:keys [handler]}]
-  
   (util/log-info "Suspending HTTP server")
-  
   (reset! handler (promise)))
 
 (defn resume 
   [k opts old-opts old-impl]
-  
   (util/log-info "Resuming HTTP server on port " (:port opts))
-  
   (if (= (dissoc opts :handler) (dissoc old-opts :handler))
     (do (deliver @(:handler old-impl) (:handler opts))
         old-impl)
